6b65c9382d323d19a97e02a667fdd0dbdd116f19
Aleksandr Vladimirovich <alex_basic@mail.ru>
2020-09-14 10:02:43 +0500

    Initial commit

---
+++ WinDockerSharpLegacy/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Net;
+
+namespace WinDockerSharpLegacy
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Application for testing of asseccing to host(local) mashine from the Docker on Windows");
+            var endPoint = "http://host.docker.internal/test/ok";
+
+            var client = new WebClient();
+
+            Console.WriteLine($"Access to endpoint: {endPoint}");
+            var okString = client.DownloadString(endPoint);
+
+            Console.WriteLine($"Success! Result: {okString}");
+        }
+    }
+}
